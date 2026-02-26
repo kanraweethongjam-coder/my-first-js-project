4c168c80272b93a38ecfae110d07e8719fce1919
Kanrawee <kanraweethongjam@gmail.com>
2026-02-26 15:00:00 +0700

    Setup HTML and CSS structure

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('toggleBtn');
+
+btn.addEventListener('click', () => {
+    // สั่งให้ body สลับคลาส dark-mode ไปมา
+    document.body.classList.toggle('dark-mode');
+    
+    // ลองเปลี่ยนข้อความในปุ่มดูด้วย
+    if (document.body.classList.contains('dark-mode')) {
+        btn.innerText = "เปลี่ยนเป็นโหมดสว่าง";
+    } else {
+        btn.innerText = "เปลี่ยนเป็นโหมดมืด";
+    }
+});

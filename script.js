2217ef3bccc998bcc8f362a614bf3f3b560dd9f2
Kanrawee <kanraweethongjam@gmail.com>
2026-02-27 13:50:07 +0700

    api-random quotes and color

--- script.js
+++ script.js
@@ -68,3 +68,16 @@ quoteBtn.addEventListener('click', getQuote);
 
 // รันครั้งแรกตอนโหลดหน้าเว็บเลย
 getQuote();
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+function changeColor(){
+    document.getElementById("quoteText").style.color = getRandomColor();
+}
